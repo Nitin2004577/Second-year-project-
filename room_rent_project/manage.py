95154610de3c640ce8d731b698d8782a2b2035d8
Nitin Kumar shah <shahnitin500@gmail.com>
2025-03-29 21:57:47 +0545

    Update manage.py

--- room_rent_project/manage.py
+++ room_rent_project/manage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import os
+import os  
 import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative taskss."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'room_rent_project.settings')
     try:
         from django.core.management import execute_from_command_line
